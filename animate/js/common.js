ca7bd67c6349faed22c2b9e5e70fab42e4ba7191
unknown <ahwangtingbiao@163.com>
2016-11-04 22:54:53 +0800

    zoom

--- animate/js/common.js
+++ animate/js/common.js
@@ -1,45 +1,23 @@
-//DIV适配
-function setSize(e1, e2, w, h, s) {
-	//获取对象
-	var e1 = document.getElementById(e1);
-	var e2 = document.getElementById(e2);
-	//取得浏览器页面可视区域的宽度
-	var screenWidth = document.documentElement.clientWidth;
-	//取得浏览器页面可视区域的高度
-	var screenHeight = document.documentElement.clientHeight;
-	//视频宽高比
-	var wh = w / h;
-	//设置显示尺寸
-	if (screenHeight < screenWidth) {
-		e1.style.width = s * w + "px";
-		e1.style.height = s * h + "px";
-		e2.style.paddingTop = (s * h - 20) / 2 + "px";
-		e1.style.marginTop = (screenHeight - s * h) / 2 + "px";
-	} else {
-		e1.style.width = s * screenWidth + "px";
-		e1.style.height = s * screenWidth / wh + "px";
-		e1.style.marginTop = (screenHeight - s * screenWidth / wh) / 2 + "px";
-		e2.style.paddingTop = (s * screenWidth / wh - 20) / 2 + "px";
-	}
-}
-
 //网页缩放
-function zoom(s) {
-	//取得浏览器页面可视区域的宽度
-	var screenHeight = document.documentElement.clientHeight;
-	//取得浏览器页面可视区域的宽度
-	var screenWidth = document.documentElement.clientWidth;
-	if (screenHeight > screenWidth) {
-		document.body.style.zoom = screenWidth / s;
-
-	}
-
+//s:设计分辨率
+function zoomBody(s) {
+    //取得浏览器页面可视区域的高度
+    var screenHeight = document.documentElement.clientHeight;
+    //取得浏览器页面可视区域的宽度
+    var screenWidth = document.documentElement.clientWidth;
+    //竖屏或者屏幕宽度小于设计分辨率s
+    if (screenHeight > screenWidth || screenWidth < s) {
+        document.body.style.zoom = screenWidth / s;
+    }
 }
 
 //添加class
+//e:对象ID,c:需要设置的class
 function myAddClass(e, c) {
-	//获取对象
-	var e = document.getElementById(e);
-	var cuclass = e.getAttribute("class");
-	e.setAttribute("class", cuclass + " " + c);
+    //获取对象
+    var e = document.getElementById(e);
+    //获取对象的当前class属性
+    var cuclass = e.getAttribute("class");
+    //设置class属性
+    e.setAttribute("class", cuclass + " " + c);
 }
