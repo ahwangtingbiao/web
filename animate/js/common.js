2958e247a5c6dce9311d9882e412e24924b609a5
unknown <ahwangtingbiao@163.com>
2016-11-05 10:18:13 +0800

    v1.0

--- animate/js/common.js
+++ animate/js/common.js
@@ -7,17 +7,18 @@ function zoomBody(s) {
     var screenWidth = document.documentElement.clientWidth;
     //竖屏或者屏幕宽度小于设计分辨率s
     if (screenHeight > screenWidth || screenWidth < s) {
+        //缩放body
         document.body.style.zoom = screenWidth / s;
     }
 }
 
 //添加class
-//e:对象ID,c:需要设置的class
+//e:对象ID,c:需要添加的class
 function myAddClass(e, c) {
     //获取对象
     var e = document.getElementById(e);
     //获取对象的当前class属性
     var cuclass = e.getAttribute("class");
-    //设置class属性
+    //添加class
     e.setAttribute("class", cuclass + " " + c);
 }
